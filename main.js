dd8beb27deda3f10f66e9d5330ad204e2ffbfde8
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-04-22 10:36:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//  Q no 88
+function roundToNearestInteger(num) {
+    return Math.round(num);
+}
+console.log(roundToNearestInteger(4.7));
+console.log(roundToNearestInteger(4.4));
+//  Qno 89
+function convertStringToNumber(str) {
+    return parseFloat(str);
+}
+console.log(convertStringToNumber("123"));
+console.log(convertStringToNumber("456"));
+//  Q no 90
+function isValueNaN(value) {
+    return isNaN(value);
+}
+console.log(isValueNaN("Hello"));
+console.log(isValueNaN(123));
